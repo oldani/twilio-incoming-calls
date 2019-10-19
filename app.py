0922cbff92172dac89d425f30fca718754ff3819
Ordanis Sanchez <ordanisanchez@gmail.com>
2019-10-19 00:22:35 -0400

    Add backend for multiple agents, and sessions

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/leave/<string:agent_id>")
+def leave(agent_id):
+    if agent_id in AVAILABALE_AGENTS:
+        AVAILABALE_AGENTS.remove(agent_id)
+    return ""
+
+
 @app.route("/twilio-webhook", methods=["POST"])
 def handle_incoming_call():
     response = VoiceResponse()
@@ -26,17 +33,26 @@ def handle_incoming_call():
         next_agent = AVAILABALE_AGENTS.popleft()
 
         dial = Dial()
-        dial.client(next_agent)
+        dial.client(str(next_agent))
         response.append(dial)
     except IndexError:
         response.say(
-            "Lo sentimos no tenemos agentes disponibles en estos momentons!",
+            "Lo sentimos, no tenemos agentes disponibles en estos momentos, llame mas tarde!",
             voice="woman",
             language="es-ES",
         )
         response.reject()
 
     return str(response)
+
+
+@app.route("/call-done/<string:agent_id>")
+def call_done(agent_id):
+    """ When agent finish a call add him back to the queue. """
+    if agent_id not in AVAILABALE_AGENTS:
+        AVAILABALE_AGENTS.append(agent_id)
+
+    return ""
 
 
 @app.route("/twilio-token")
